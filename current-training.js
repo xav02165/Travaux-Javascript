527b3dc14b9c9411b7f0b2e851df093dcc236a83
xav02135 <xav.dx02@gmail.com>
2025-06-04 15:24:03 +0200

    hj

---
+++ current-training.js
@@ -0,0 +1,404 @@
+/*const cellule = document.querySelectorAll(."case");
+
+
+
+let Resultat = document.getElementById("");
+let scoreX = 0;
+let scoreO = 0;
+
+
+/* Fonction definit qui gagne celon le tableau 
+function Gagnant() {
+  for (let i = 0; i < victoire.length; i++) {
+    if (
+      document.getElementById(victoire[i][0]).innerHTML === symbole &&
+      document.getElementById(victoire[i][1]).innerHTML === symbole &&
+      document.getElementById(victoire[i][2]).innerHTML === symbole
+    ) {
+
+/* Affichage du gagnant selon les symboles  
+      if (symbole === 'X') {
+        Resultat.innerHTML = 'Le gagnant est le joueur 1';
+        
+      } else {
+        Resultat.innerHTML = 'Le gagnant est le joueur 2';
+        
+      }
+      /*pour arreter l'affichage des symboles
+
+      finJeu = true;
+    }
+  }
+  
+}*/
+const case0 = document.querySelector('.case0')
+const case1 = document.querySelector('.case1')
+const case2 = document.querySelector('.case2')
+const case3 = document.querySelector('.case3')
+const case4 = document.querySelector('.case4')
+const case5 = document.querySelector('.case5')
+const case6 = document.querySelector('.case6')
+const case7 = document.querySelector('.case7')
+const case8 = document.querySelector('.case8')
+
+let turnPlayer = 0;
+let compteur = 0;
+
+case0.addEventListener('click', () => {
+    if (compteur < 9) {
+        compteur++
+        return
+    }
+    if (compteur == 9) {
+        console.log ("match nul")
+        return
+    }
+})
+case1.addEventListener('click', () => {
+    if (compteur < 9) {
+        compteur++
+        return
+    }
+    if (compteur == 9) {
+        console.log ("match nul")
+        return
+    }
+})
+case2.addEventListener('click', () => {
+    if (compteur < 9) {
+        compteur++
+        return
+    }
+    if (compteur == 9) {
+        console.log ("match nul")
+        return
+    }
+})
+case3.addEventListener('click', () => {
+    if (compteur < 9) {
+        compteur++
+        return
+    }
+    if (compteur == 9) {
+        console.log ("match nul")
+        return
+    }
+})
+case4.addEventListener('click', () => {
+    if (compteur < 9) {
+        compteur++
+        return
+    }
+    if (compteur == 9) {
+        console.log ("match nul")
+        return
+    }
+})
+case5.addEventListener('click', () => {
+    if (compteur < 9) {
+        compteur++
+        return
+    }
+    if (compteur == 9) {
+        console.log ("match nul")
+        return
+    }
+})
+case6.addEventListener('click', () => {
+    if (compteur < 9) {
+        compteur++
+        return
+    }
+    if (compteur == 9) {
+        console.log ("match nul")
+        return
+    }
+})
+case7.addEventListener('click', () => {
+    if (compteur < 9) {
+        compteur++
+        return
+    }
+    if (compteur == 9) {
+        console.log ("match nul")
+        return
+    }
+})
+case8.addEventListener('click', () => {
+    if (compteur < 9) {
+        compteur++
+        return
+    }
+    if (compteur == 9) {
+        console.log ("match nul")
+        return
+    }
+})
+
+
+
+
+case0.addEventListener('click', () => {
+
+    if (case0.style.backgroundImage !== ""){
+        return
+    }
+
+    if (turnPlayer == 0){
+        case0.style.backgroundImage = "url('images/x.png')";
+        turnPlayer = 1
+    }
+    else {
+        case0.style.backgroundImage = "url('images/o.png')";
+        turnPlayer = 0     
+    }
+
+verifVictoire()
+
+
+})
+case1.addEventListener('click', () => {
+    if (case1.style.backgroundImage !== ""){
+        return
+    }
+    if (turnPlayer == 0){
+        case1.style.backgroundImage = "url('images/x.png')";
+        turnPlayer = 1
+    }
+    else {
+        case1.style.backgroundImage = "url('images/o.png')";
+        turnPlayer = 0     
+    }
+    
+    if (case0.style.backgroundImage && case1.style.backgroundImage && case2.style.backgroundImage == 'url("images/x.png")'){
+        console.log("c'est gagne");
+    }
+
+
+verifVictoire()
+
+
+
+
+})
+case2.addEventListener('click', () => {
+    if (case2.style.backgroundImage !== ""){
+        return
+    }
+    if (turnPlayer == 0){
+        case2.style.backgroundImage = "url('images/x.png')";
+        turnPlayer = 1
+    }
+    else {
+        case2.style.backgroundImage = "url('images/o.png')";
+        turnPlayer = 0     
+    }
+    
+        if (case0.style.backgroundImage && case1.style.backgroundImage && case2.style.backgroundImage == 'url("images/x.png")'){
+        console.log("c'est gagne");
+        
+    }
+
+    verifVictoire()
+})
+case3.addEventListener('click', () => {
+    if (case3.style.backgroundImage !== ""){
+        return
+    }    
+    if (turnPlayer == 0){
+        case3.style.backgroundImage = "url('images/x.png')";
+        turnPlayer = 1
+    }
+    else {
+        case3.style.backgroundImage = "url('images/o.png')";
+        turnPlayer = 0     
+    }
+
+    verifVictoire()
+    
+})
+case4.addEventListener('click', () => {
+    if (case4.style.backgroundImage !== ""){
+        return
+    }
+    if (turnPlayer == 0){
+        case4.style.backgroundImage = "url('images/x.png')";
+        turnPlayer = 1
+    }
+    else {
+        case4.style.backgroundImage = "url('images/o.png')";
+        turnPlayer = 0     
+    }
+    
+    verifVictoire()
+})
+
+case5.addEventListener('click', () => {
+    if (case5.style.backgroundImage !== ""){
+        return
+    }
+    if (turnPlayer == 0){
+        case5.style.backgroundImage = "url('images/x.png')";
+        turnPlayer = 1
+    }
+    else {
+        case5.style.backgroundImage = "url('images/o.png')";
+        turnPlayer = 0     
+    }
+
+    verifVictoire()
+    
+})
+case6.addEventListener('click', () => {
+    if (case6.style.backgroundImage !== ""){
+        return
+    }
+    if (turnPlayer == 0){
+        case6.style.backgroundImage = "url('images/x.png')";
+        turnPlayer = 1
+    }
+    else {
+        case6.style.backgroundImage = "url('images/o.png')";
+        turnPlayer = 0     
+    }
+    
+    verifVictoire()
+})
+
+case7.addEventListener('click', () => {
+    if (case7.style.backgroundImage !== ""){
+        return
+    }
+    if (turnPlayer == 0){
+        case7.style.backgroundImage = "url('images/x.png')";
+        turnPlayer = 1
+    }
+    else {
+        case7.style.backgroundImage = "url('images/o.png')";
+        turnPlayer = 0     
+    }
+    verifVictoire()
+})
+
+case8.addEventListener('click', () => {
+    if (case8.style.backgroundImage !== ""){
+        return
+    }
+    if (turnPlayer == 0){
+        case8.style.backgroundImage = "url('images/x.png')";
+        turnPlayer = 1
+    }
+    else {
+        case8.style.backgroundImage = "url('images/o.png')";
+        turnPlayer = 0     
+    }
+    verifVictoire()
+})
+
+
+function verifVictoire(){
+    // Condition de victoire
+    // Victoire pour les X
+    if ((case0.style.backgroundImage === 'url("images/x.png")' && case1.style.backgroundImage === 'url("images/x.png")' && case2.style.backgroundImage) === 'url("images/x.png")'){      
+        console.log("c'est gagne pour les X")
+        scoreX++ 
+        compteurVictoire();
+        
+        
+    }
+    if (case3.style.backgroundImage === 'url("images/x.png")' && case4.style.backgroundImage === 'url("images/x.png")' && case5.style.backgroundImage === 'url("images/x.png")'){
+        console.log("c'est gagne pour les X")
+        scoreX++
+        compteurVictoire();
+    }
+    if (case6.style.backgroundImage === 'url("images/x.png")' && case7.style.backgroundImage === 'url("images/x.png")' && case8.style.backgroundImage === 'url("images/x.png")'){
+        console.log("c'est gagne pour les X")
+        scoreX++
+        compteurVictoire();
+    }
+    if (case0.style.backgroundImage === 'url("images/x.png")' && case3.style.backgroundImage === 'url("images/x.png")' && case6.style.backgroundImage === 'url("images/x.png")'){
+        console.log("c'est gagne pour les X")
+        scoreX++
+        compteurVictoire();
+    }
+    if (case1.style.backgroundImage === 'url("images/x.png")' && case4.style.backgroundImage === 'url("images/x.png")' && case7.style.backgroundImage === 'url("images/x.png")'){
+        console.log("c'est gagne pour les X")
+        scoreX++
+        compteurVictoire();
+    }
+    if (case2.style.backgroundImage === 'url("images/x.png")' && case5.style.backgroundImage === 'url("images/x.png")' && case8.style.backgroundImage === 'url("images/x.png")'){
+        console.log("c'est gagne pour les X")
+        scoreX++
+        compteurVictoire();
+    }
+    if (case0.style.backgroundImage === 'url("images/x.png")' && case4.style.backgroundImage === 'url("images/x.png")' && case8.style.backgroundImage === 'url("images/x.png")'){
+        console.log("c'est gagne pour les X")
+        scoreX++
+        compteurVictoire();
+    }
+    if (case2.style.backgroundImage === 'url("images/x.png")' && case4.style.backgroundImage === 'url("images/x.png")' && case6.style.backgroundImage === 'url("images/x.png")'){
+        console.log("c'est gagne pour les X")
+        scoreX++
+        compteurVictoire();
+    }
+
+    // Victoire pour les O
+    if (case0.style.backgroundImage === 'url("images/o.png")' && case1.style.backgroundImage === 'url("images/o.png")' && case2.style.backgroundImage === 'url("images/o.png")'){
+        console.log("c'est gagne pour les O")
+        scoreO++
+        compteurVictoire();
+    }
+    if (case3.style.backgroundImage === 'url("images/o.png")' && case4.style.backgroundImage === 'url("images/o.png")' && case5.style.backgroundImage === 'url("images/o.png")'){
+        console.log("c'est gagne pour les O")
+        scoreO++
+        compteurVictoire();
+    }
+    if (case6.style.backgroundImage === 'url("images/o.png")' && case7.style.backgroundImage === 'url("images/o.png")' && case8.style.backgroundImage === 'url("images/o.png")'){
+        console.log("c'est gagne pour les O")
+        scoreO++
+        compteurVictoire();
+    }
+    if (case0.style.backgroundImage === 'url("images/o.png")' && case3.style.backgroundImage  === 'url("images/o.png")'&& case6.style.backgroundImage === 'url("images/o.png")'){
+        console.log("c'est gagne pour les O")
+        scoreO++
+        compteurVictoire();
+    }
+    if (case1.style.backgroundImage === 'url("images/o.png")' && case4.style.backgroundImage === 'url("images/o.png")' && case7.style.backgroundImage === 'url("images/o.png")'){
+        console.log("c'est gagne pour les O")
+        scoreO++
+        compteurVictoire();
+    }
+    if (case2.style.backgroundImage === 'url("images/o.png")' && case5.style.backgroundImage === 'url("images/o.png")' && case8.style.backgroundImage === 'url("images/o.png")'){
+        console.log("c'est gagne pour les O")
+        scoreO++
+        compteurVictoire();
+    }
+    if (case0.style.backgroundImage === 'url("images/o.png")' && case4.style.backgroundImage === 'url("images/o.png")' && case8.style.backgroundImage === 'url("images/o.png")'){
+        console.log("c'est gagne pour les O")
+        scoreO++
+        compteurVictoire();
+    }
+    if (case2.style.backgroundImage === 'url("images/o.png")' && case4.style.backgroundImage === 'url("images/o.png")' && case6.style.backgroundImage === 'url("images/o.png")'){
+        console.log("c'est gagne pour les O")
+        scoreO++
+        compteurVictoire();
+    }
+    
+
+}
+
+/*compteur de victoire*/
+let scoreX = 0;
+let scoreO = 0;
+
+function compteurVictoire() {
+    
+    let spanX = document.getElementById("scoreX");
+    spanX.textContent = scoreX;
+    
+        
+    let spanO = document.getElementById("scoreO");
+    spanO.textContent = scoreO;
+        
+}
+
