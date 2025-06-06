9b8e25bae444d52f0c12ee79b5a01cf89067ec37
xav02135 <xav.dx02@gmail.com>
2025-06-06 16:24:21 +0200

    fin semaine exercice

--- Revision js/rev.js
+++ Revision js/rev.js
@@ -1,10 +1,53 @@
 // nombre de 1 a 100
 
-let nombre = 7;
+/*let nombre = 7;
 let tentative = 0;
 
 while (tentative !== nombre) {
     tentative = parseInt(prompt("devinez un nombre entre 1 et 100 :"));
+
+    console.log("ce n'est pas le bon nombre" );
+}
+console.log("bravo, vous avez deviné le nombre!");*/
+
+
+//calendrier
+
+/*function infoMois(Nombrejours) {
+    const mois = [
+        { nom: "janvier", jour: 31 },
+        { nom: "fevrier", jour: 28 },
+        { nom: "mars", jour: 31 },
+        { nom: "avril", jour: 30},
+        { nom: "mai", jour: 31 },
+        { nom: "juin", jour: 30 },
+        { nom: "juillet", jour: 31 },
+        { nom: "aout", jour: 31 },
+        { nom: "septembre", jour: 30 },
+        { nom: "octobre", jour: 31 },
+        { nom: "novembre", jour: 30 },
+        { nom: "decembre", jour: 31},
+    ];
+}*/
+
+/*let compteur = 1 ;
+let sommeTotal = 0;
+
+while (compteur<=100) {
+    sommeTotal = compteur + sommeTotal++;
+    compteur++
+    
+}
+
+  console.log(sommeTotal);*/
+
+   //nombre de 1 a 10
+
+let nombre = 7;
+let tentative = 0;
+
+while (tentative !== nombre) {
+    tentative = parseInt(prompt("devinez un nombre entre 1 et 10 :"));
 
     console.log("ce n'est pas le bon nombre" );
 }
