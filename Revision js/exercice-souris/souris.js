5c41ea5cfd3a7a46723d126a486317e883c6a05f
xav02135 <xav.dx02@gmail.com>
2025-06-10 16:24:59 +0200

    souris fin

--- Revision js/exercice-souris/souris.js
+++ Revision js/exercice-souris/souris.js
@@ -10,6 +10,8 @@ document.addEventListener ( 'mousemove', (event) => {
     const x = event.clientX; 
     const y = event.clientY; 
     console.log(`Position de la souris : X=${x}, Y=${y}`);
+    mouse.style.left = x + "px" ;
+    mouse.style.top = y + "px";
 
 })
 
