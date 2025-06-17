98434ccde3f0ec9d45171339fa9c61f3ea49086d
xav02135 <xav.dx02@gmail.com>
2025-06-17 16:07:22 +0200

    ajout mep

--- jeu du pendu solo/pendu.js
+++ jeu du pendu solo/pendu.js
@@ -1,18 +1,14 @@
-
 const ens1 = document.querySelector('.ens1');
 const ens2 = document.querySelector('.ens2');
 const ens3 = document.querySelector('.ens3');
 const ens4 = document.querySelector('.ens4');
 const ens5 = document.querySelector('.ens5');
-
 const ens6 = document.querySelector('.ens6');
 const ens7 = document.querySelector('.ens7');
 const ens8 = document.querySelector('.ens8');
 const ens9 = document.querySelector('.ens9');
 const ens10 = document.querySelector('.ens10');
 const h2 = document.querySelector('h2');
-
-
 
 // Mot à deviner
 const motADeviner = prompt("Entrez un mot pour le jeu du pendu :");
@@ -83,21 +79,14 @@ function verifierLettre(event) {
     
         case 0:
         ens10.style.backgroundColor = "black" ;
-        
         break;
-
-        
-     }
+    }
     
-
-
-
-  // Affichage
+    // Affichage
   affichageMot.textContent = lettresTrouvees.join(" ");
   affichageEssais.textContent = `Essais restants : ${essaisRestants}`;
 
-
-  // Vérifie les conditions de victoire ou de défaite
+    // Vérifie les conditions de victoire ou de défaite
   if (!lettresTrouvees.includes("_")) {
     alert("Félicitations, vous avez gagné !");
     document.removeEventListener("keydown", verifierLettre);
