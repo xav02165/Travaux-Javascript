4f3869a848ee12eb6c7e9a88612855380a430c0b
xav02135 <xav.dx02@gmail.com>
2025-06-19 11:59:18 +0200

    exercice rickandmorty

---
+++ exercicecours/js.js
@@ -0,0 +1,22 @@
+    
+    
+    let i = 0;
+    let x = 1;
+
+    while (i < 826 ) {
+    fetch('https://rickandmortyapi.com/api/character/' + x)
+    .then((response) => response.json())
+    .then(data => {
+        const img = document.createElement('img');
+        img.src = data.image;
+        img.alt = data.name;
+        img.style.width = '200px';
+        img.style.height = '200px';
+        img.style.border = ' 1 px solid black; margin: 10px;';
+        document.body.appendChild(img);
+        document.body.appendChild(document.createTextNode(data.name));
+    })
+    .catch(error => console.error('Error fetching data:', error));
+    x++;
+    i++;
+}
