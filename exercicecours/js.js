c359640eec5ed37dde5d46d1e5f00b0a3bbdd5cf
xav02135 <xav.dx02@gmail.com>
2025-06-19 14:51:23 +0200

    ajout br

--- exercicecours/js.js
+++ exercicecours/js.js
@@ -10,10 +10,10 @@
         const img = document.createElement('img');
         img.src = data.image;
         img.alt = data.name;
-        img.style.width = '200px';
-        img.style.height = '200px';
-        img.style.borderColor = ' 1 px solid black; margin: 10px;';
-        document.body.appendChild(img);
+        img.style.width = '300px';
+        img.style.height = '300px';
+        document.body.appendChild(img) 
+        document.body.appendChild(document.createElement('br')) 
         document.body.appendChild(document.createTextNode(data.name));
     })
     .catch(error => console.error('Error fetching data:', error));
