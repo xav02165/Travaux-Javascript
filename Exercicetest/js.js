f107b2605a19ea0f9a9e7af8593c2bc58be9582e
xav02135 <xav.dx02@gmail.com>
2025-06-24 14:12:30 +0200

    essai

--- Exercicetest/js.js
+++ Exercicetest/js.js
@@ -1,8 +1,37 @@
-const formulaire = document.querySelector('form');
+/*const formulaire = document.querySelector('form');
 const btnSubmit = document.querySelector('button');
 
-formulaire.addEventListener('submit', function(event) {
+
+
+btnSubmit.addEventListener('submit', function(event) {
     event.preventDefault(); 
 
     
+    })*/
+
+const Form = document.getElementById('mockform');
+Form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+
+    const firstName = document.getElementById('firstname').value;
+    const lastName = document.getElementById('lastname').value;
+    
+
+  fetch('https://685a52c59f6ef9611155e0fe.mockapi.io/demo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prenom: firstName,
+      nom: lastName
     })
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    alert(`Utilisateur créé avec succès : ${JSON.stringify(data)}`);
+  })
+});
