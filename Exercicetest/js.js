f2d57352bf78f67e4aa5f2d70ee76787008c8b81
xav02135 <xav.dx02@gmail.com>
2025-06-24 15:32:22 +0200

    debug

--- Exercicetest/js.js
+++ Exercicetest/js.js
@@ -10,28 +10,30 @@ btnSubmit.addEventListener('submit', function(event) {
     })*/
 
 const Form = document.getElementById('mockform');
-Form.addEventListener('submit', function(e) {
+
+Form.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    const firstName = document.getElementById('firstname');
+    const lastName = document.getElementById('lastname');
+    console.log(firstName.value);
+    console.log(lastName.value);
 
-    const firstName = document.getElementById('firstname').value;
-    const lastName = document.getElementById('lastname').value;
-    
-
-  fetch('https://685a52c59f6ef9611155e0fe.mockapi.io/demo', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      prenom: firstName,
-      nom: lastName
+    fetch('https://685a97779f6ef9611157086a.mockapi.io/api/v1', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            prenom: firstName.value,
+            nom: lastName.value,
+        })
     })
-  })
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    alert(`Utilisateur créé avec succès : ${JSON.stringify(data)}`);
-  })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            alert(`Utilisateur créé avec succès : ${JSON.stringify(data)}`);
+        })
 });
+
